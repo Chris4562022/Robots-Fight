0e7b989bd2bea88a8dc83e265d33f4f59bcd514d
Chris4562022 <williamschris83@gmail.com>
2022-04-19 12:59:41 -0400

    updating the javasctript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+//  note the lack of quotation marks around playerName
+
+//What's this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//This will do match and log 20
+console.log(10 + 10);
+//What is this?
+console.log("Our Robot's name is " + playerName);
+
+//fight();
